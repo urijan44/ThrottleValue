d04c405fff6ad041cfc8fdcb1c79d583aaa37e43
urijan44 <teamsva360@gmail.com>
2022-11-02 00:35:56 +0900

    ThrottleValue int

--- Sources/ThrottleValue/ThrottleValue.swift
+++ Sources/ThrottleValue/ThrottleValue.swift
@@ -1,6 +1,46 @@
-public struct ThrottleValue {
-    public private(set) var text = "Hello, World!"
+//
+//  ThrottleValue.swift
+//  
+//
+//  Created by hoseung Lee on 2022/11/01.
+//
 
-    public init() {
+import Foundation
+
+@propertyWrapper
+public struct ThrottleValue<Value> {
+    final class InTime {
+        var inProgress = false
     }
+    
+    private var inTime = InTime()
+    private var value: Value
+    
+    let interval: TimeInterval
+    public var wrappedValue: Value {
+        get {
+            value
+        }
+        
+        set {
+            if !inTime.inProgress {
+                value = newValue
+                inTime.inProgress = true
+                setTimer()
+            }
+        }
+    }
+    
+    public init(_ value: Value, interval: TimeInterval) {
+        self.value = value
+        self.interval = interval
+    }
+    
+    mutating
+    private func setTimer() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + interval) { [weak inTime] in
+            inTime?.inProgress = false
+        }
+    }
+    
 }
--- Tests/ThrottleValueTests/ThrottleValueTests.swift
+++ Tests/ThrottleValueTests/ThrottleValueTests.swift
@@ -2,10 +2,51 @@ import XCTest
 @testable import ThrottleValue
 
 final class ThrottleValueTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ThrottleValue().text, "Hello, World!")
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+    
+    
+    func test_ignore_value() {
+        @ThrottleValue(0, interval: 1) var value: Int
+        value = 1
+        let exp1 = expectation(description: "1")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            value = 2
+            exp1.fulfill()
+        }
+        
+        let exp2 = expectation(description: "2")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
+            value = 3
+            exp2.fulfill()
+        }
+        
+        let exp3 = expectation(description: "3")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.9) {
+            value = 4
+            exp3.fulfill()
+        }
+        
+        wait(for: [exp1, exp2, exp3], timeout: 1)
+        XCTAssertEqual(value, 1)
+    }
+    
+    func test_write_value() {
+        @ThrottleValue(0, interval: 1) var value: Int
+        value = 1
+        let exp = expectation(description: "1")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.1) {
+            value = 5
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 2)
+        XCTAssertEqual(value, 5)
     }
 }
